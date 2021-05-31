8eed01e80ed6758239bef25611cff1932fd6542c
b10730026 <b10730026.gapps.ntust.edu.tw>
2021-06-01 01:05:44 +0800

    更改畫面

--- main/index.php
+++ main/index.php
@@ -81,7 +81,7 @@
         <div class="vertical-center">
             <h1 class="quote">熱門項目</h1>
         </div>
-        <div class="horizon-between wrap">
+        <div class="horizon-items wrap">
             <div class="project-object">
                 <div class="image-container">
                     <img src="../images/product/chicken.jpg">
